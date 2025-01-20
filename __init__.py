5d963f630cc59df5e0c5b7e9ef0fc4a3678dd37e
WonkManGood <ezrajwadding@gmail.com>
2025-01-19 21:22:01 -0500

    'Commited via script.'

--- __init__.py
+++ __init__.py
@@ -6,8 +6,9 @@ from wonkLED_package.led_interface import (
 __all__ = [simpleLED, rgbLED]
 
 try:
-    with open(file='.wonkKEY', mode='r'): ...
+    with open(file='/.wonkKEY', mode='r'): ...
     from sys import path
+    print('Imported')
     path.append('/home/ej/wonkLIB')
 except BaseException: ...
 print(dir())
